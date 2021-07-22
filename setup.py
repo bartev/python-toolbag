5191b3cd59a20937f967ffd1212e838b0a820515
Bartev Vartanian <bartev@apple.com>
2021-07-22 15:07:13 -0500

    Init commit (from cookiecutter)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Snippets of stuff I've learned in python',
+    author='Bartev',
+    license='',
+)
